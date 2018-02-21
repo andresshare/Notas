4eca8e60b94f42b4e081bf7a1758e7c1c251329e
andresshare <andresshare@gmail.com>
2018-02-20 21:28:43 -0500

    files.py

---
+++ Notas/Notas.py
@@ -0,0 +1,33 @@
+"""
+Strings
+"""
+
+cadena = "I'm String"
+
+print(len(cadena))
+print(cadena[0])
+print (cadena[-1])
+print(cadena[1:4])
+print(cadena[:5])
+
+palabra = 'python'
+palabra = 'super'+palabra[:2]
+print(palabra)
+
+#string
+print("Today I had {0} cups of {1}".format(3,"coffee"))
+print('{:<20}'.format("text"))
+print('{:>20}'.format("text"))
+
+print("'I\'m a string in Python")
+
+message = 'text1'
+
+''' IF'''
+
+if message == 'text':
+    print('Hi')
+else:
+    print('bye')
+
+    '''staments'''
---
+++ Notas/files/filetwo.py
@@ -0,0 +1,5 @@
+pets = ['cat', 'dog', 'fish']
+marks = [5, 4, 3, 2, 1]
+
+for f in pets:
+    print(f)    
